c44da2e2be71bcf848dd59210347136e4fce91e9
Michael Young <msyoung2012@gmail.com>
2022-08-05 11:13:16 -0400

    Installing custom fonts
    Got a little crazy. To make this work, remember to `npm start ios` after adding new fonts. Clearing the cache alone is not enough to solve the `unrecognized font family` error.

--- foodappui/App.js
+++ foodappui/App.js
@@ -98,6 +98,7 @@ const styles = StyleSheet.create({
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
+    fontFamily: 'Montserrat-Regular',
   },
   sectionDescription: {
     marginTop: 8,
