451c33e46d87ce46774f9370c8a8b04c6262b848
Michael Young <msyoung2012@gmail.com>
2022-08-06 09:51:27 -0400

    importing additional vector icons

--- foodappui/components/Home.js
+++ foodappui/components/Home.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import {Text} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default Home = () => {
   return <Text>Home page</Text>;
