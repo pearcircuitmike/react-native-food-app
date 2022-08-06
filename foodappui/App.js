99880e10ec6e8b8eeb7c7b0ee9d10096739c5797
Michael Young <msyoung2012@gmail.com>
2022-08-06 09:40:37 -0400

    Set up nav dependencies
    Using reactnavigation.org

--- foodappui/App.js
+++ foodappui/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import {Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <Text> Welcome to my food app :) </Text>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
